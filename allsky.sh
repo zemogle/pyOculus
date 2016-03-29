32ad9c52d784fb6875697785e930729db7478197
Edward Gomez <edward@gomez.me.uk>
2016-03-29 22:51:15 +0100

    Bash cmds to make movies

--- allsky.sh
+++ allsky.sh
@@ -6,7 +6,9 @@ convert -quality 90% -resize 75% /home/pi/images/latest.png /home/pi/images/late
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.json s3://www.zemogle.uk/allsky/
 
 # Next make a list of all .png files created in the last 12 hours
-#find . -type f -name '*.png' -cmin -720
+find 20*.png -type f -cmin -720 -printf "file '%f'\n" > image_files.txt
 
 # Make a movie out of them
-#ffmpeg -f image2 -pattern_type glob -i '*.png' -pix_fmt yuv420p -vcodec h264 latest_allsky.mpv
+ffmpeg -f concat -i image_files.txt  -pix_fmt yuv420p -vcodec h264 latest_allsky.mov -y
+/usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.mov s3://www.zemogle.uk/allsky/
+mv $oldfile /home/pi/images/latest_$(date +%F-%T).mov
