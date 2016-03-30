0fd163511aceaabf390ebf09c28662573ce2b34b
Edward Gomez <edward@gomez.me.uk>
2016-03-30 23:11:00 +0100

    Fixing ffmpeg

--- allsky.sh
+++ allsky.sh
@@ -5,10 +5,8 @@ convert -quality 90% -resize 75% /home/pi/images/latest.png /home/pi/images/late
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.jpg s3://www.zemogle.uk/allsky/
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.json s3://www.zemogle.uk/allsky/
 
-# Next make a list of all .png files created in the last 12 hours
-find /home/pi/images/20*.png -type f -cmin -720 -printf "file '%f'\n" > /home/pi/images/image_files.txt
+# Next make a list of all .png files created in the last 12 hours & Make a movie out of them
+find /home/pi/images/20*.png -type f -cmin -720 -exec cat {} \; | ffmpeg -f image2pipe -r 1  -i - -vcodec libx264 latest.mov -y
 
-# Make a movie out of them
-cd /home/pi/images/ ;ffmpeg -f concat -i /home/pi/images/image_files.txt  -pix_fmt yuv420p -vcodec h264 /home/pi/images/latest.mov -y
-/usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.mov s3://www.zemogle.uk/allsky/
+/usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync /home/pi/images/latest.mov s3://www.zemogle.uk/allsky/
 mv /home/pi/images/latest.mov /home/pi/images/latest_$(date +%F).mov
