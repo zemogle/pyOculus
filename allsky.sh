1546e6b73b9f381da882bb936ced7ebf5da96dbe
Edward Gomez <edward@gomez.me.uk>
2016-03-30 10:19:00 +0100

    Updating script

--- allsky.sh
+++ allsky.sh
@@ -6,9 +6,9 @@ convert -quality 90% -resize 75% /home/pi/images/latest.png /home/pi/images/late
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.json s3://www.zemogle.uk/allsky/
 
 # Next make a list of all .png files created in the last 12 hours
-find 20*.png -type f -cmin -720 -printf "file '%f'\n" > image_files.txt
+find 20*.png -type f -cmin -720 -printf "file '%f'\n" > /home/pi/images/image_files.txt
 
 # Make a movie out of them
-ffmpeg -f concat -i image_files.txt  -pix_fmt yuv420p -vcodec h264 latest_allsky.mov -y
+ffmpeg -f concat -i /home/pi/images/image_files.txt  -pix_fmt yuv420p -vcodec h264 /home/pi/images/latest.mov -y
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.mov s3://www.zemogle.uk/allsky/
-mv $oldfile /home/pi/images/latest_$(date +%F-%T).mov
+mv /home/pi/images/latest.mov /home/pi/images/latest_$(date +%F).mov
