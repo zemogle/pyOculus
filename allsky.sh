b43b38691b31d0158326884607152c87a8575511
Edward Gomez <edward@gomez.me.uk>
2016-04-07 19:21:28 +0100

    Use environment for cron

--- allsky.sh
+++ allsky.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python /home/pi/pyOculus/snapper.py >/home/pi/allsky.log
+source /home/pi/env/allsky/bin/activate; python /home/pi/pyOculus/snapper.py >/home/pi/allsky.log
 convert -quality 90% -resize 75% /home/pi/images/latest.png /home/pi/images/latest.jpg
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.jpg s3://www.zemogle.uk/allsky/
 /usr/local/bin/s3cmd -c /home/pi/.s3cfg --no-progress sync images/*.json s3://www.zemogle.uk/allsky/
